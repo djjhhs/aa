89c4a066282199077c771c130a1138d33e34d893
jingbo <1559146155@qq.com>
2021-08-13 10:24:13 +0800

    fiest-commit

---
+++ src/main/java/com/git/test/first.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+/**
+ * @author jingbo
+ * @create 2021--08--13  10:21
+ */
+public class first {
+    public static void main(String[] args) {
+        System.out.println("耗时啊");
+    }
+}
